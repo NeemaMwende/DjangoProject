18591f6a66446cce75dfe21ddb38f3624f61098b
NeemaMwende <gracepete12@gmail.com>
2025-05-27 19:10:59 +0300

    update: retrieve, update, destroy

--- backend/api/views.py
+++ backend/api/views.py
@@ -28,10 +28,21 @@ class ProjectViewset(viewsets.ViewSet):
             return Response(serializer.errors, status=400)
 
     def retrieve(self, request, pk=None):
-        pass
+        project = self.queryset.get(pk=pk)
+        serializer = self.serializer_class(project)
+        return Response(serializer.data)
 
     def update(self, request, pk=None):
-        pass
+        project = self.queryset.get(pk=pk)
+        serializer = self.serializer_class(project, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=400)
 
     def destroy(self, request, pk=None):
-        pass
+        project = self.queryset.get(pk=pk)
+        project.delete()
+        return Response(status=204)
+    
