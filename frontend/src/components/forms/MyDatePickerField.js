b5659a9df4d5680b9df9fad95a4bbf03932fb4ff
NeemaMwende <gracepete12@gmail.com>
2025-05-29 15:31:18 +0300

    updates: components

--- frontend/src/components/forms/MyDatePickerField.js
+++ frontend/src/components/forms/MyDatePickerField.js
@@ -1,15 +1,21 @@
 import * as React from "react";
-import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { Controller } from "react-hook-form";
 
-export default function MyDatePickerField() {
+export default function MyDatePickerField(props) {
+  const { label, name, control } = props;
+
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DemoContainer components={["DatePicker"]}>
-        <DatePicker label="Basic date picker" />
-      </DemoContainer>
+      <Controller
+        name={name}
+        control={control}
+        render={({ field: { onChange, value } }) => (
+          <DatePicker label="Basic date picker" />
+        )}
+      />
     </LocalizationProvider>
   );
 }
